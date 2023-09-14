9d544493011b49f9a623f038f30049bd738c850f
yashsinggg <134526548+yashsinggg@users.noreply.github.com>
2023-09-14 16:28:04 +0530

    Auth Setup

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
-'use client'
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './globals.css';
-import Container from 'react-bootstrap/Container';
-import InvoiceForm from './components/InvoiceForm';
+"use client";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+import Container from "react-bootstrap/Container";
+import InvoiceForm from "./components/InvoiceForm";
+
+import { UserButton } from "@clerk/nextjs";
 
 function App() {
   return (
     <div className="App d-flex flex-column align-items-center justify-content-center w-100">
+      <UserButton afterSignOutUrl="/" />
+
       <Container>
         <InvoiceForm />
       </Container>
