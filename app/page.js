49cd8497e57792064b8a1049ad7c31e65e892672
yashsinggg <134526548+yashsinggg@users.noreply.github.com>
2023-09-14 19:57:25 +0530

    Added NavBar

--- app/page.js
+++ app/page.js
@@ -9,8 +9,22 @@ import { UserButton } from "@clerk/nextjs";
 
 function App() {
   return (
-    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
-      <UserButton afterSignOutUrl="/" />
+    <div className="App d-flex w-full flex-column align-items-center justify-content-center">
+      <nav class="bg-white border-gray-200 dark:bg-gray-900 w-full">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="/" class="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              class="h-25 mr-300 ml-30"
+              alt="Flowbite Logo"
+            />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-Black ml-30">
+              Goverment Invoice
+            </span>
+          </a>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </nav>
 
       <Container>
         <InvoiceForm />
